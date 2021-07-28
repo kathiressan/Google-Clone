fc0887bd419229ce5401defc56bfc01ce03c6c5c
unknown <kathiressan26pro@gmail.com>
2021-07-28 18:41:22 +0800

    First commit

---
+++ keys.js
@@ -0,0 +1,7 @@
+// API Key - https://developers.google.com/custom-search/v1/using_rest
+const API_KEY = " ";
+
+// Context Key - https://cse.google.com/cse/create/new
+const CONTEXT_KEY = "";
+
+export { API_KEY, CONTEXT_KEY };
